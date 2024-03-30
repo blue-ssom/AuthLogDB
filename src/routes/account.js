55a4452d2b246816e0d5a06c65caa2c509030304
blue-ssom <somi5404@naver.com>
2024-03-30 01:59:23 +0000

    implement password retrieval functionality

--- src/routes/account.js
+++ src/routes/account.js
@@ -40,6 +40,42 @@ router.get('/find-id', async(req, res) => {
 });
 
 // 비밀번호 찾기
+router.get('/find-password', async(req, res) => {
+    const { id, name, phoneNumber} = req.body
+    const result = {
+        "success" : false,
+        "message" : "",
+        "data" : null
+    }
+
+    try {
+
+        // 예외처리
+        utils.checkRequiredField(id, "아이디")
+        utils.checkName(name, "이름")
+        utils.checkPhoneNumber(phoneNumber, "전화번호")
+
+        // DB통신
+        const sql = `SELECT password FROM scheduler.user WHERE id = $1 AND name = $2 AND phoneNumber = $3`;
+        const data = await pool.query(sql, [id, name, phoneNumber]);
+
+        // DB 후처리
+        const row = data.rows
+
+        if(row.length === 0){
+            throw new Error("회원정보가 존재하지 않습니다.")
+        }
+        
+        result.success = true
+        result.message = "비밀번호 찾기 성공";
+        result.data = row[0].password;
+
+   } catch (e) {
+       result.message = e.message;
+   } finally {
+       res.send(result);
+   }
+});
 // 회원가입
 // 특정 user 정보 보기
 // 내 회원 정보 수정
